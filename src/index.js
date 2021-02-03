20b21ff6b3698b522a8182328017550041f46cff
Soubhagya <soubhagyaranjanghs@gmail.com>
2021-02-03 21:17:55 +0530

    created add note functionality

--- src/index.js
+++ src/index.js
@@ -15,6 +15,3 @@ ReactDOM.render(<App />, document.getElementById("root"));
 //- Callback from the Note component to trigger a delete function.
 //- Use the filter function to filter out the item that needs deletion.
 //- Pass a id over to the Note component, pass it back to the App when deleting.
-
-//This is the end result you're aiming for:
-//https://pogqj.csb.app/
